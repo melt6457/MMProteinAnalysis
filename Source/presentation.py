8fa0077de06488d554611ca7e5020216ec13cab6
Kory Melton <melt6457@pacificu.edu>
2017-10-31 20:03:55 -0700

    Updated Presentation.py
    to graph stuffs

--- Source/presentation.py
+++ Source/presentation.py
@@ -9,6 +9,8 @@ simNames = pa.selectMultipleSimulations()
 simNames2 = pa.selectMultipleSimulations()
 sims = []
 sims2 = []
+simNames3 = pa.selectMultipleSimulations()
+sims3 = []
 
 for count in range(0, len(simNames)):
     fileSet = files.getSimulationFiles(simNames[count])
@@ -34,15 +36,34 @@ for count in range(0, len(simNames2)):
 for num in range(1, len(simNames2)):
     sims2[0].combine(sims2[num])
 
-sims2[0].rmsd.calculateFrameTimes(0, sims2[0].log.getEndTime() - sims2[0].log.getStartTime())
+# sims2[0].rmsd.calculateFrameTimes(0, sims2[0].log.getEndTime() - sims2[0].log.getStartTime())
 # plt.plot(sims2[0].rmsd.times, sims2[0].rmsd.RMSDs)
 plt.plot(sims2[0].log.times, sims2[0].log.pot_energy)
 
+for count in range(0, len(simNames)):
+    fileSet = files.getSimulationFiles(simNames[count])
+    sim3 = pa.createSimAnalysis(fileSet)
+    sim3.loadLog()
+    sim3.loadRMSD()
+    sims3.append(sim3)
+
+for count in range(1, len(simNames)):
+    sims3[0].combine(sims[count])
+
+plt.plot(sims3[0].log.times, sims3[0].log.pot_energy)
+
+avePotEnergy = sims[0].calcAvePotentialEnergy()
+avePotEnergy2 = sims2[0].calcAvePotentialEnergy()
+avePotEnergy3 = sims3[0].calcAvePotentialEnergy()
+print("The Average Potential Energy of sim 1 is: ", avePotEnergy)
+print("The Average Potential Energy of sim 2 is: ", avePotEnergy2)
+print("The Average Potential Energy of sim 3 is: ", avePotEnergy3)
+
 plt.show()
 
-print('Goodbye')
+'''print('Data')
 
-'''aveDAT = sims[0].calcAveDAT()
+aveDAT = sims[0].calcAveDAT()
 stdDevDAT = sims[0].calcStdDevDAT()
 
 print("The Average of the 300 Data is: ", aveDAT)
@@ -56,11 +77,23 @@ stdDevDATs.append(stdDevDAT)
 
 objects = ('300 K', '500 K')
 
-aveDAT = sims2[0].calcAveDAT()
-stdDevDAT = sims2[0].calcStdDevDAT()
+aveDAT2 = sims2[0].calcAveDAT()
+stdDevDAT2 = sims2[0].calcStdDevDAT()
 
-print("The Average of the 300 Data is: ", aveDAT)
-print("The Standard Deviation of the 300 Data:", stdDevDAT)
+print("The Average of the 300 Data is: ", aveDAT2)
+print("The Standard Deviation of the 300 Data:", stdDevDAT2)
+
+aveDAT2 = sims2[0].calcAveDAT()
+stdDevDAT2 = sims2[0].calcStdDevDAT()
+
+print("The Average of the 300 Data is: ", aveDAT2)
+print("The Standard Deviation of the 300 Data:", stdDevDAT2)
+
+aveDAT3 = sims3[0].calcAveDAT()
+stdDevDAT3 = sims3[0].calcStdDevDat()
+
+print("The Average of the 500 Data is: ", aveDAT3)
+print("The Standard Deviation of the 500 Data:", stdDevDAT3)
 
 aveDATs.append(aveDAT)
 stdDevDATs.append(stdDevDAT)
